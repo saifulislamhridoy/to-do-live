f31443a0da09a80b35bf031a0c01d33d2a41844b
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-06-30 18:34:52 +0600

    added dynamic route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import ToDo from './components/ToDo/ToDo'
+import CompleteTask from './components/CompleteTask/CompleteTask'
+import Calender from './components/Calender/Calender'
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/todo" element={<ToDo></ToDo>}></Route>
+          <Route path="/completetask" element={<CompleteTask></CompleteTask>}></Route>
+          <Route path="/calender" element={<Calender></Calender>}></Route>
+        </Routes>
+      </Navbar>
     </div>
   );
 }
---
+++ src/components/CompleteTask/CompleteTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompleteTask = () => {
+    return (
+        <div>
+            <h2>This is Complete Task</h2>
+        </div>
+    );
+};
+
+export default CompleteTask;
